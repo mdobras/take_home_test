637b53f8631ea6190f34f18fc65bf193f26a8e03
Michael Dobras <dobras.michael@gmail.com>
2022-06-16 06:05:06 +0200

    Initial Commit

---
+++ roomapp/roomappApp.swift
@@ -0,0 +1,17 @@
+//
+//  roomappApp.swift
+//  roomapp
+//
+//  Created by miko on 16.06.22.
+//
+
+import SwiftUI
+
+@main
+struct roomappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
